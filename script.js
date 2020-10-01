57cb8ff41e68dd057862adf45c2c2241d06597bb
dvramos <dvramos@hotmail.com.br>
2020-10-01 14:43:46 -0300

    direções, impedimento de sumiço, food e aumento da snake

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ snake[0]={
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+    }
 
 function criarBG() {
 context.fillStyle = "lightgreen";
@@ -18,5 +23,55 @@ function criarCobrinha(){
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
-criarBG();
-criarCobrinha();
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x,food.y, box, box);
+}
+
+document.addEventListener('keydown',update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right")     direction = "left";
+    if(event.keyCode == 38 && direction != "down")      direction = "up";
+    if(event.keyCode == 39 && direction != "left")      direction = "right";
+    if(event.keyCode == 40 && direction != "up")        direction = "down";
+}
+
+function iniciarJogo(){
+    if (snake[0].x > 15 * box && direction == "right")  snake[0].x=0;
+    if (snake[0].x < 0  * box && direction == "left")   snake[0].x=16*box;
+    if (snake[0].y > 15 * box && direction == "down")   snake[0].y=0;
+    if (snake[0].y < 0  * box && direction == "up")     snake[0].y=16*box;
+
+    criarBG();
+    criarCobrinha();
+    drawFood();
+
+    let snakeX = snake[0].x; 
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box,
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
+    
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100);
